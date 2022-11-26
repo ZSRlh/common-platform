747ee8894daefe2ff7300eb768e0b0ea64677566
ZSRlh <952177238@qq.com>
2022-11-26 17:42:25 +0800

    fix: vercel config

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,7 @@
-import { BASEURL } from "@/assets/constants/constants";
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  let target = BASEURL;
+  let target = 'http://rest.apizza.net/mock/c93fdfb30dab4ca2008a92a4497f29eb';
   createProxyMiddleware({
     target,
     changeOrigin: true,

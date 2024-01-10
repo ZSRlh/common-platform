85d88f59943fe818af0a973bcbd92f01508f74ce
zsr <zsr_lh@163.com>
2024-01-10 11:20:12 +0800

    fix: currnetMenu 类型

--- src/stores/menu.ts
+++ src/stores/menu.ts
@@ -1,34 +1,34 @@
 import { reactive, toRefs, watch } from "vue";
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 import type { MenuDataItem } from "@/types/menu";
-import { getMenus } from '@/api';
+import { getMenus } from "@/api";
 
 export const useMenuStore = defineStore("menuStore", () => {
   const menus = reactive<{
-    headerMenu: MenuDataItem[],
-    sideMenu: MenuDataItem[],
-    currentHeaderNav: String[],
+    headerMenu: MenuDataItem[];
+    sideMenu: MenuDataItem[];
+    currentHeaderNav: string[];
   }>({
     headerMenu: [],
     sideMenu: [],
     currentHeaderNav: [],
-  })
+  });
 
   const _getMenus = async () => {
-    getMenus({ userName: 'zsr' }).then(res => {
+    getMenus({ userName: "zsr" }).then((res) => {
       menus.headerMenu = res.menu;
     });
   };
 
-  const setHeaderNav = (menuKey: String) => {
+  const setHeaderNav = (menuKey: string) => {
     menus.currentHeaderNav = [menuKey];
-  }
-  
+  };
+
   // 选中标签变化时，更新子菜单
   watch(
     () => menus.currentHeaderNav,
     (val) => {
-      const menuObj = menus.headerMenu.find(e => e?.key === val[0]);
+      const menuObj = menus.headerMenu.find((e) => e?.key === val[0]);
       menus.sideMenu = menuObj?.children ? menuObj.children : [];
     },
     { immediate: true }
@@ -41,11 +41,11 @@ export const useMenuStore = defineStore("menuStore", () => {
       menus.currentHeaderNav = [val[0]?.key];
     },
     { immediate: true }
-  )
+  );
 
   return {
     ...toRefs(menus),
     getMenus: _getMenus,
     setHeaderNav,
   };
-})
+});
